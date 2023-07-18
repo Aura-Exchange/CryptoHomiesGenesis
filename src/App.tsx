5ea5a6a1e6f39398504622eccc78137152d890f3
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-07-18 14:45:44 -0500

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from 'react';
+import React, { useMemo, useState, useEffect } from 'react';
 import { BigNumber, ethers, utils } from 'ethers';
 import {
   ConnectWallet,
@@ -24,7 +24,6 @@ import {
   primaryColorConst,
   themeConst,
 } from './consts/parameters';
-import { CrossmintPayButton } from '@crossmint/client-sdk-react-ui';
 
 const urlParams = new URL(window.location.toString()).searchParams;
 const contractAddress =
@@ -48,6 +47,7 @@ const colors = {
 
 export default function Home() {
   const [desiredTokenAmount, setDesiredTokenAmount] = useState(1);
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (event: { target: { value: string; }; }) => {
     let value = parseInt(event.target.value);
@@ -82,23 +82,12 @@ export default function Home() {
     contractQuery.contract,
     address || ''
   );
-  const claimIneligibilityReasons = useClaimIneligibilityReasons(
-    contractQuery.contract,
-    {
-      quantity,
-      walletAddress: address || '',
-    }
-  );
   const unclaimedSupply = useUnclaimedNFTSupply(contractQuery.contract);
   const claimedSupply = useClaimedNFTSupply(contractQuery.contract);
   const { data: firstNft, isLoading: firstNftLoading } = useNFT(
     contractQuery.contract,
     0
   );
-
-  const numberClaimed = useMemo(() => {
-    return BigNumber.from(claimedSupply.data || 0).toString();
-  }, [claimedSupply]);
 
   const { contract: totalSupplyContract } = useContract(
     '0x5f8eD33d9eC6B28DAafa9A1f9faDff3D9f94e5fB'
@@ -108,112 +97,6 @@ export default function Home() {
     'totalSupply',
     []
   );
-
-  const priceToMint = useMemo(() => {
-    const bnPrice = BigNumber.from(
-      activeClaimCondition.data?.currencyMetadata.value || 0
-    );
-    return `${utils.formatUnits(
-      bnPrice.mul(quantity).toString(),
-      activeClaimCondition.data?.currencyMetadata.decimals || 18
-    )} ${activeClaimCondition.data?.currencyMetadata.symbol}`;
-  }, [
-    activeClaimCondition.data?.currencyMetadata.decimals,
-    activeClaimCondition.data?.currencyMetadata.symbol,
-    activeClaimCondition.data?.currencyMetadata.value,
-    quantity,
-  ]);
-
-  const maxClaimable = useMemo(() => {
-    let bnMaxClaimable;
-    try {
-      bnMaxClaimable = BigNumber.from(
-        activeClaimCondition.data?.maxClaimableSupply || 0
-      );
-    } catch (e) {
-      bnMaxClaimable = BigNumber.from(1_000_000);
-    }
-
-    let perTransactionClaimable;
-    try {
-      perTransactionClaimable = BigNumber.from(
-        activeClaimCondition.data?.maxClaimablePerWallet || 0
-      );
-    } catch (e) {
-      perTransactionClaimable = BigNumber.from(1_000_000);
-    }
-
-    if (perTransactionClaimable.lte(bnMaxClaimable)) {
-      bnMaxClaimable = perTransactionClaimable;
-    }
-
-    const snapshotClaimable = claimerProofs.data?.maxClaimable;
-
-    if (snapshotClaimable) {
-      if (snapshotClaimable === '0') {
-        // allowed unlimited for the snapshot
-        bnMaxClaimable = BigNumber.from(1_000_000);
-      } else {
-        try {
-          bnMaxClaimable = BigNumber.from(snapshotClaimable);
-        } catch (e) {
-          // fall back to default case
-        }
-      }
-    }
-
-    const maxAvailable = BigNumber.from(unclaimedSupply.data || 0);
-
-    let max;
-    if (maxAvailable.lt(bnMaxClaimable)) {
-      max = maxAvailable;
-    } else {
-      max = bnMaxClaimable;
-    }
-
-    if (max.gte(1_000_000)) {
-      return 1_000_000;
-    }
-    return max.toNumber();
-  }, [
-    claimerProofs.data?.maxClaimable,
-    unclaimedSupply.data,
-    activeClaimCondition.data?.maxClaimableSupply,
-    activeClaimCondition.data?.maxClaimablePerWallet,
-  ]);
-
-  const isSoldOut = useMemo(() => {
-    try {
-      return (
-        (activeClaimCondition.isSuccess &&
-          BigNumber.from(
-            activeClaimCondition.data?.availableSupply || 0
-          ).lte(0)) ||
-        numberClaimed === numberTotal
-      );
-    } catch (e) {
-      return false;
-    }
-  }, [
-    activeClaimCondition.data?.availableSupply,
-    activeClaimCondition.isSuccess,
-    numberClaimed,
-    numberTotal,
-  ]);
-
-  const canClaim = useMemo(() => {
-    return (
-      activeClaimCondition.isSuccess &&
-      claimIneligibilityReasons.isSuccess &&
-      claimIneligibilityReasons.data?.length === 0 &&
-      !isSoldOut
-    );
-  }, [
-    activeClaimCondition.isSuccess,
-    claimIneligibilityReasons.data?.length,
-    claimIneligibilityReasons.isSuccess,
-    isSoldOut,
-  ]);
 
   const isLoading = useMemo(() => {
     return (
@@ -229,68 +112,35 @@ export default function Home() {
     unclaimedSupply.isLoading,
   ]);
 
-  const buttonLoading = useMemo(
-    () => isLoading || claimIneligibilityReasons.isLoading,
-    [claimIneligibilityReasons.isLoading, isLoading]
-  );
-
-  const buttonText = useMemo(() => {
-    if (isSoldOut) {
-      return 'Sold Out';
+  useEffect(() => {
+    if (claimConditions.isError) {
+      setErrorMessage('Failed to fetch claim conditions.');
     }
+  }, [claimConditions.isError]);
 
-    if (canClaim) {
-      const pricePerToken = BigNumber.from(
-        activeClaimCondition.data?.currencyMetadata.value || 0
-      );
-      if (pricePerToken.eq(0)) {
-        return 'Mint (Free)';
-      }
-      return `Mint (${priceToMint})`;
+  useEffect(() => {
+    if (activeClaimCondition.isError) {
+      setErrorMessage('Failed to fetch active claim condition.');
     }
-    if (claimIneligibilityReasons.data?.length) {
-      return parseIneligibility(
-        claimIneligibilityReasons.data,
-        quantity
-      );
+  }, [activeClaimCondition.isError]);
+
+  useEffect(() => {
+    if (claimerProofs.isError) {
+      setErrorMessage('Failed to fetch claimer proofs.');
     }
-    if (buttonLoading) {
-      return 'Checking eligibility...';
+  }, [claimerProofs.isError]);
+
+  useEffect(() => {
+    if (unclaimedSupply.isError) {
+      setErrorMessage('Failed to fetch unclaimed NFT supply.');
     }
+  }, [unclaimedSupply.isError]);
 
-    return 'Minting not available';
-  }, [
-    isSoldOut,
-    canClaim,
-    claimIneligibilityReasons.data,
-    buttonLoading,
-    activeClaimCondition.data?.currencyMetadata.value,
-    priceToMint,
-    quantity,
-  ]);
-
-  const dropNotReady = useMemo(
-    () =>
-      claimConditions.data?.length === 0 ||
-      claimConditions.data?.every(
-        (cc) => cc.maxClaimableSupply === '0'
-      ),
-    [claimConditions.data]
-  );
-
-  const dropStartingSoon = useMemo(
-    () =>
-      (claimConditions.data &&
-        claimConditions.data.length > 0 &&
-        activeClaimCondition.isError) ||
-      (activeClaimCondition.data &&
-        activeClaimCondition.data.startTime > new Date()),
-    [
-      activeClaimCondition.data,
-      activeClaimCondition.isError,
-      claimConditions.data,
-    ]
-  );
+  useEffect(() => {
+    if (claimedSupply.isError) {
+      setErrorMessage('Failed to fetch claimed NFT supply.');
+    }
+  }, [claimedSupply.isError]);
 
   if (!contractAddress) {
     return (
@@ -306,22 +156,21 @@ export default function Home() {
 
       <div className="grid h-screen grid-cols-1 lg:grid-cols-12">
         <div className="hidden h-full w-full items-center justify-center lg:col-span-5 lg:flex lg:px-12">
-          <HeadingImage
+          <img
             src="https://bafybeifrdu43ddcecl5a4ipjrkrpfrperstxcpiuoicucxffp3l76ry7qy.gateway.ipfscdn.io/chg.gif"
-            isLoading={isLoading}
           />
         </div>
         <div className="col-span-1 flex h-full w-full items-center justify-center lg:col-span-7">
           <div className="flex w-full max-w-xl flex-col gap-4 rounded-xl p-12 lg:border lg:border-gray-400 lg:dark:border-gray-800">
             <div className="mt-8 flex w-full xs:mb-8 xs:mt-0 lg:hidden">
-              <HeadingImage
+              <img
                 src="https://bafybeifrdu43ddcecl5a4ipjrkrpfrperstxcpiuoicucxffp3l76ry7qy.gateway.ipfscdn.io/chg.gif"
-                isLoading={isLoading}
               />
             </div>
 
+            {/* {errorMessage && <p>{errorMessage}</p>} */}
+
             <div className="flex flex-col gap-2 xs:gap-4">
-              {isLoading ? (
                 <div
                   role="status"
                   className="animate-pulse space-y-8 md:flex md:items-center md:space-x-8 md:space-y-0"
@@ -330,16 +179,14 @@ export default function Home() {
                     <div className="h-10 w-24 rounded-full bg-gray-200 dark:bg-gray-700"></div>
                   </div>
                 </div>
-              ) : (
                 <p>
                   <span className="text-lg font-bold tracking-wider text-gray-500 xs:text-xl lg:text-2xl">
                     {numberTotal}
                   </span>
                   <span className="text-lg font-bold tracking-wider xs:text-xl lg:text-2xl">
-                    / {"1998"} Minted!
+                    / 1998 Minted!
                   </span>
                 </p>
-              )}
               <h1 className="line-clamp-1 text-2xl font-bold xs:text-3xl lg:text-4xl">
                 {contractMetadata.isLoading ? (
                   <div
@@ -356,7 +203,7 @@ export default function Home() {
                 )}
               </h1>
               {contractMetadata.data?.description ||
-              contractMetadata.isLoading ? (
+                contractMetadata.isLoading ? (
                 <div className="line-clamp-2 text-gray-500">
                   {contractMetadata.isLoading ? (
                     <div
@@ -380,26 +227,121 @@ export default function Home() {
                 <div className="flex w-full flex-col gap-4 lg:flex-row lg:items-center lg:gap-4 ">
                   <Web3Button
                     contractAddress="0x5f8eD33d9eC6B28DAafa9A1f9faDff3D9f94e5fB"
-                    action={(contract) => {
-                      contract.call('mint', [address, desiredTokenAmount], {
-                        value: ethers.utils.parseEther(
-                          (0.1515 * desiredTokenAmount).toString()
-                        ),
-                      });
-                    }}
-                    onSuccess={() => {
-                      toast({
-                        title: 'Successfully minted',
-                        description:
-                          'Congrats! Your Crypto Homies Genesis NFT\'s are on their way!',
-                        duration: 5000,
-                        className: 'bg-green-500',
-                      });
+                    action={async (contract) => {
+                      try {
+                        const tx = await contract.call(
+                          'mint',
+                          [address, desiredTokenAmount],
+                          {
+                            value: ethers.utils.parseEther(
+                              (0.1515 * desiredTokenAmount).toString()
+                            ),
+                          }
+                        );
+                        await tx.wait(); // Wait for the transaction to be mined
+
+                        toast({
+                          title: 'Successfully minted',
+                          description: 'The NFT has been transferred to your wallet! To view your new NFT please visit https://hub.auraexchange.org/collection/ethereum/0x5f8ed33d9ec6b28daafa9a1f9fadff3d9f94e5fb',
+                          duration: 5000,
+                          className: 'bg-green-500',
+                        });
+                      } catch (err: any) {
+                        console.error(err);
+                        console.log({ err });
+
+                        const errorMessage = err.message;
+
+                        if (errorMessage.includes('value:     0.1515 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 1 Genesis NFT is 0.1515 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     0.303 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 2 Genesis NFTs is 0.303 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     0.4545 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 3 Genesis NFTs is 0.4545 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     0.606 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 4 Genesis NFTs is 0.606 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     0.7575 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 5 Genesis NFTs is 0.7575 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     0.909 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 6 Genesis NFTs is 0.909 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     1.0605 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 7 Genesis NFTs is 1.0605 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     1.212 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 8 Genesis NFTs is 1.212 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     1.3635 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 9 Genesis NFTs is 1.3635 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else if (errorMessage.includes('value:     1.515 ETH')) {
+                          toast({
+                            title: 'Transaction Error',
+                            description: "To mint 10 Genesis NFTs is 1.515 plus gas. You don't have enough funds to mint. Please add more funds and try again.",
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        } else {
+                          toast({
+                            title: 'Transaction Error',
+                            description: err.reason || '',
+                            duration: 9000,
+                            variant: 'destructive',
+                          });
+                        }
+
+                        // Log the error message to the console
+                        console.error(errorMessage);
+                      }
                     }}
                   >
                     Mint
                   </Web3Button>
-                  <br/>
+
+
+
+                  <br />
                   <label htmlFor="tokenAmount">Number of Genesis:</label>
                   <select
                     id="tokenAmount"
@@ -412,7 +354,7 @@ export default function Home() {
                       </option>
                     ))}
                   </select>
-                  <br/>
+                  <br />
                   <button
                     onClick={handleCheckoutClick}
                     style={{
